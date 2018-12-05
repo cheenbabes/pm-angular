d9fb0623d85c3393e834cf449ed420d9aa6ecc07
evgeniibaibourine <eugene.baibourine@tanium.com>
2018-12-05 17:28:20 -0500

    working giver form and stats

--- app/home/home.js
+++ app/home/home.js
@@ -2,16 +2,57 @@
 
 angular.module('myApp.home', [])
 
-.controller('HomeController', ['$scope', function($scope) {
-	var giversRef = db.collection('givers');
-	$scope.givers =[];
+	.controller('HomeController', ['$scope', function ($scope) {
+		var giversRef = db.collection('givers');
+		var usersRef = db.collection('users');
+		$scope.givers = [];
+		$scope.users = [];
 
-	giversRef.onSnapshot(function(querySnapshot){
-		$scope.givers=[];
-		querySnapshot.forEach(function(doc){
-			$scope.$apply(function() {
-				$scope.givers.push(doc.data());
-			})
-		})
-	}) 
-}]);
+		// get givers and force apply scope
+		giversRef.onSnapshot(function (querySnapshot) {
+			$scope.givers = [];
+			querySnapshot.forEach(function (doc) {
+				$scope.$apply(function () {
+					$scope.givers.push(doc.data());
+				});
+			});
+		});
+
+		// get users (temples) and force apply scope
+		usersRef.onSnapshot(function (querySnapshot) {
+			$scope.givers = [];
+			querySnapshot.forEach(function (doc) {
+				$scope.$apply(function () {
+					$scope.users.push(doc.data());
+				});
+			});
+		});
+
+		$scope.submitGiver = function () {
+			var toCommit = {
+				name: $scope.giverName,
+				email: $scope.giverEmail,
+				country: $scope.giverCountry
+			};
+			navigator.geolocation.getCurrentPosition(function (position) {
+				$scope.longitude = position.coords.longitude;
+				$scope.latitude = position.coords.latitude;
+
+				toCommit.longitude = $scope.longitude;
+				toCommit.latitude = $scope.latitude;
+				console.log(toCommit);
+				db.collection('givers').doc(toCommit.email).set(toCommit);
+			}, function (err) {
+				console.log("Geolocation was blocked, submitting without geolocation");
+				db.collection('givers').doc(toCommit.email).set(toCommit);
+			});
+		}
+
+		$scope.daysLeft = function () {
+			var end = new Date('January 1, 2019');
+			var now = Date.now();
+
+			return Math.round((end - now) / (1000 * 60 * 60 * 24));
+
+		}
+	}]);
