ce210f2debdbeceb08698b0174e115e71936cdb7
evgeniibaibourine <eugene.baibourine@tanium.com>
2018-12-12 14:09:35 -0500

    add authed route

--- app/app.js
+++ app/app.js
@@ -8,8 +8,9 @@ angular.module('myApp', [
   'myApp.login',
   'toaster',
   'ngAnimate'
-]).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+])
+
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$locationProvider.hashPrefix('');
   $routeProvider
 
@@ -21,12 +22,37 @@ config(['$routeProvider', '$locationProvider', function($routeProvider, $locatio
     templateUrl: '/login/login.html',
     controller: 'LoginController'
   })
+  .when('/book-form', {
+    templateUrl: '/book/book.html',
+    controller: 'BookFormController',
+    resolve: {
+      user: ['$http', '$q', function($http, $q){
+        var deferred = $q.defer();
+        if(firebase.auth().currentUser){
+          deferred.resolve(firebase.auth().currentUser);
+        } else {
+          deferred.reject('NOT_AUTHORIZED');
+        }
+        return deferred.promise;
+      }]
+    }
+  })
   .otherwise({redirectTo: '/'});
+}])
+
+.run(['$rootScope', '$location', function($rootScope, $location){
+  $rootScope.$on('$routeChangeError', function(event, current, previous, rejection){
+    if(rejection === 'NOT_AUTHORIZED'){
+      $location.path('/login');
+    }
+  })
 }]);
+
 
 var db = firebase.firestore();
 db.settings({
   timestampsInSnapshots: true
 });
 var auth = firebase.auth();
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 
